ff99666f3a9d400602f75805f5e3155cf0ffc3c3
charles <charles.delmaire@gmail.com>
2014-01-09 10:28:06 -0500

    Ajout utilisateur

---
+++ Site/tinymvc/myapp/views/view-ajoutUtilisateur.php
@@ -0,0 +1,74 @@
+<div class="panel medium-12 columns">
+	<h3>Ajout d'un utilisateur</h3>
+	
+	<form>
+  <div class="row">
+    <div class="large-5 columns">
+      <label>Nom</label>
+      <input type="text" placeholder="large-5.columns" />
+    </div>
+	<div class="large-5 columns">
+      <label>Prénom</label>
+      <input type="text" placeholder="large-5.columns" />
+    </div>
+  </div>
+  <div class="row">
+    <div class="large-5 columns">
+      <label>Mot de Passe</label>
+      <input type="text" placeholder="large-5.columns" />
+    </div>
+	<div class="large-5 columns">
+      <label>Courriel</label>
+      <input type="text" placeholder="large-5.columns" />
+    </div>
+  </div>
+  
+	<div class="row">
+		<div class="large-3 columns">
+			<label>Numéro civique</label>
+			<input type="text" placeholder="large-4.columns" />
+		</div>
+		<div class="large-5 columns">
+			<label>Rue</label>
+			<input type="text" placeholder="large-5.columns" />
+		</div>
+	</div>
+	<div class="row">
+		<div class="large-5 columns">
+			<label>Ville</label>
+			<input type="text" placeholder="large-4.columns" />
+		</div>
+		<div class="large-5 columns">
+			<label>Code postal</label>
+			<input type="text" placeholder="large-4.columns" />
+		</div>
+	</div>
+	<div class="row">
+		<div class="large-4 columns">
+		  <label>possesseur d'un clé</label>
+		  <select>
+			<option value="Non">Non</option>
+			<option value="Oui">Oui</option>
+		  </select>
+		</div>
+		<div class="large-4 columns">
+		  <label>Type employé</label>
+		  <select>
+			<option value="Non">Employé</option>
+			<option value="Oui">Gestionnaire</option>
+		  </select>
+		</div>
+		<div class="large-4 columns">
+			<label>Responsable Conflit</label>
+			<input type="radio" name="RespCon" value="non" id="rcNon" Checked><label for="rcNon" >Non</label>
+			<input type="radio" name="RespCon" value="oui" id="rcOui"><label for="rcOui">Oui</label>
+		</div>
+	</div>
+
+ 
+ 
+  
+  
+</form>
+	<a href="#" class="button right radius">Envoyer</a>
+</div>
