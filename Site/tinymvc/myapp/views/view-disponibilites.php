9c3eab660ac74057b9e75c874ecafa0bac9e3161
samuel <samuel@gmail.com>
2014-01-09 09:08:30 -0500

    Liste deroulante

--- Site/tinymvc/myapp/views/view-disponibilites.php
+++ Site/tinymvc/myapp/views/view-disponibilites.php
@@ -1,5 +1,58 @@
 <div class="medium-12 columns">
 	<h3>Saisie des disponibilités</h3>
+	
+	<script type="text/javascript">
+		var myDays= ["Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"];
+		var mois = [ "Janvier", "Février", "Mars", "Avril", "Mai", "Juin",
+				   "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre" ];
+						 
+		function startAndEndOfWeek(date)
+		{
+		  // If no date object supplied, use current date
+		  // Copy date so don't modify supplied date
+		  var now = date? new Date(date) : new Date();
+		  // Get the previous Dimanche
+		  var Dimanche = new Date(now);
+		  Dimanche.setDate(Dimanche.getDate() - Dimanche.getDay());
+
+		  // Get next Sunday
+		  var Samedi = new Date(now);
+		  Samedi.setDate(Samedi.getDate() - Samedi.getDay() + 6);
+			
+		  var dateSemaineDebut = Dimanche.getDate().toString() + " " + mois[Dimanche.getMonth()] + " " + Dimanche.getFullYear(); 
+		  var dateSemaineFin = Samedi.getDate().toString() + " " + mois[Samedi.getMonth()] + " " + Samedi.getFullYear();
+		  // Return array of date objects
+		  return [dateSemaineDebut, dateSemaineFin];
+		}
+		
+		function remplirListeDate()
+		{
+			var date = new Date();
+			var vecteurDateSemaine = [];
+			
+			for(i = 5; i >= 0; i--)
+			{
+				vecteurDateSemaine.push(startAndEndOfWeek(date - (7 * i)));
+			}
+			
+			for(i = 0; i <= 10; i++)
+			{
+				vecteurDateSemaine.push(startAndEndOfWeek(date + (7 * i)));
+			}
+			
+			//Récupérer la liste qui existe
+			var uneListeDate = document.getElementById("listeDate");
+				
+			for(i = 0; i < vecteurDateSemaine.length; i++)
+			{
+				var option = document.createElement("option");
+				option.text = vecteurDateSemaine[i][0] + vecteurDateSemaine[i][1];
+				uneListeDate.options.add(option);
+			}
+		}
+		remplirListeDate();
+	</script>
+	
 	<table id="horaire">
 		<thead>
 			<tr>
