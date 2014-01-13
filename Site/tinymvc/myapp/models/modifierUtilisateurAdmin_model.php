0a07b473a17e1f023db608f5dc273e4f94e88dee
charles <charles.delmaire@gmail.com>
2014-01-13 15:26:58 -0500

    ff

---
+++ Site/tinymvc/myapp/models/modifierUtilisateurAdmin_model.php
@@ -0,0 +1,18 @@
+<?php
+	class modifierUtilisateurAdmin_Model extends TinyMVC_Model
+	{
+		function modifierUnutilisateur($courriel, $nom, $prenom, $motPasse, $numerocivique, $rue, $ville, $codePostal,  $possesseurCle, $typeEmploye, $formationVetement, $formationChaussure, $formationCaissier, $respHoraireConflit, $notifHoraire,  $notifRemplacement) {
+
+			$row = $this->db->query_one('Call ModifierUtilisateurAdmin( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', array($courriel, $nom, $prenom, $motPasse, $numerocivique, $rue, $ville, $codePostal,  $possesseurCle, $typeEmploye, $formationVetement, $formationChaussure, $formationCaissier, $respHoraireConflit, $notifHoraire,  $notifRemplacement));
+		
+    			$arrayUtil = array (
+					"nom"  => $row["nom"],
+					"prenom" => $row["prenom"]);
+				return $arrayUtil;
+    		
+			
+			
+			
+		}
+	}
+?>
