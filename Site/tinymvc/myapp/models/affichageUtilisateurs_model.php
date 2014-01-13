7eb9b765098027984e8dce402daf2a5931418981
Marcleking <marcantoine.bouchardm@gmail.com>
2014-01-13 12:50:02 -0500

    SDF

--- Site/tinymvc/myapp/models/affichageUtilisateurs_model.php
+++ Site/tinymvc/myapp/models/affichageUtilisateurs_model.php
@@ -5,9 +5,6 @@
 			$row = $this->db->query('Call Utilisateurs()');
 			while($row = $this->db->next()) {
 				if ($row["courriel"] != $_SESSION['user']->getNom()) {
-					echo $row["courriel"];
-					echo "<br />";
-					echo $_SESSION['user']->getNom();
 					$results[] = $row;
 				}
 			}
